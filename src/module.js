3d6bea4a4fc74c45a314465ba372ef6408b2b4b5
Christian <cmkramer@gmail.com>
2015-10-15 19:20:00 +0200

    Make sure the dist is built without sourcemaps and lose dependency to the translation lib

--- src/module.js
+++ src/module.js
@@ -22,4 +22,4 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-angular.module('cf.feedback',['pascalprecht.translate']);
+angular.module('cf.feedback',[]);
